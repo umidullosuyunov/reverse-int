e48af41a3f179d3c3f75dc29296d617581d63f7b
Umidullo Suyunov <suyunovumidullo@gmail.com>
2020-10-22 13:24:26 +0500

    feat: complete reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = reverse = (n) => {
+  if (n > 0) {
+    return n.toString().split("").reverse().join("");
+  } else {
+    return n.toString().slice(1).split("").reverse().join("");
+  }
+};
